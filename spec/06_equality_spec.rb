8cb416e3c6c5110cab7eb94188d79c0df9b77c12
parentda <danielparent21@gmail.com>
2021-09-21 18:36:46 -0400

    completed exercise 06

--- spec/06_equality_spec.rb
+++ spec/06_equality_spec.rb
@@ -67,21 +67,26 @@ describe 'equality assignment' do
   let(:amy) { { fav_color: 'blue', fav_food: 'tacos' } }
   let(:bob) { { fav_color: 'blue', fav_food: 'tacos' } }
   let(:copy_cat) { amy }
+
   # Write a test that expresses each of the following statements.
 
   # remove the 'x' before running this test
-  xit 'amy is eq to bob' do
+  it 'amy is eq to bob' do
+    expect(amy).to eq(bob)
   end
 
   # remove the 'x' before running this test
-  xit 'amy is eql to bob' do
+  it 'amy is eql to bob' do
+    expect(amy).to eql(bob)
   end
 
   # remove the 'x' before running this test
-  xit 'amy is not equal to bob' do
+  it 'amy is not equal to bob' do
+    expect(amy).not_to be(bob)
   end
 
   # remove the 'x' before running this test
-  xit 'copy_cat is equal to amy' do
+  it 'copy_cat is equal to amy' do
+    expect(copy_cat).to be(amy)
   end
 end
