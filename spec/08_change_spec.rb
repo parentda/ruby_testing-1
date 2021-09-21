3e1ef72190beb264fd1f72e6a616953fe32a5842
parentda <danielparent21@gmail.com>
2021-09-21 19:22:14 -0400

    completed exercise 08

--- spec/08_change_spec.rb
+++ spec/08_change_spec.rb
@@ -41,7 +41,9 @@ describe Array do
 
     # You can compound change matchers together.
     it 'will decrease by one and end with tea' do
-      expect { drinks.pop }.to change { drinks.length }.by(-1).and change { drinks.last }.to('tea')
+      expect { drinks.pop }.to change { drinks.length }.by(-1).and change {
+                          drinks.last
+                        }.to('tea')
     end
   end
 end
@@ -50,20 +52,26 @@ end
 
 describe 'lucky numbers with rotate! method' do
   subject(:lucky_numbers) { [3, 7, 13, 31, 42] }
+
   # Write a test that expresses each of the following statements.
   # Use the array #rotate! method to manipulate lucky_numbers.
 
   context 'when rotating the array of lucky numbers' do
     # remove the 'x' before running this test
-    xit 'will change the first value to 7' do
+    it 'will change the first value to 7' do
+      expect { lucky_numbers.rotate! }.to change { lucky_numbers.first }.to(7)
     end
 
     # remove the 'x' before running this test
-    xit 'will change the last value to 3' do
+    it 'will change the last value to 3' do
+      expect { lucky_numbers.rotate! }.to change { lucky_numbers.last }.to(3)
     end
 
     # remove the 'x' before running this test
-    xit 'will change the first value to 7 and last value to 3' do
+    it 'will change the first value to 7 and last value to 3' do
+      expect { lucky_numbers.rotate! }.to change { lucky_numbers.first }.to(
+        7
+      ).and change { lucky_numbers.last }.to(3)
     end
   end
 end
